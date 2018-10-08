8d88ef38bb16dc717b14bcfc3e9ac790764e0efd
Neel Raiyani <nraiyani@gmail.com>
2018-10-08 13:35:19 -0400

    Memory Leak Detection

--- Direct3D_11_Exe/main.cpp
+++ Direct3D_11_Exe/main.cpp
@@ -1,13 +1,19 @@
+#ifdef _DEBUG
+// CRT Memory Leak detection
+#define _CRTDBG_MAP_ALLOC  
+#include <stdlib.h>  
+#include <crtdbg.h>
+#endif
+
 #include "application.h"
-
-
-//#include <string>
-//#include <Windows.h>
 
 
 auto main() -> int
 {
-	//std::wstring cmd_line(GetCommandLine());
+#ifdef _DEBUG
+	// Detects memory leaks upon program exit
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
 	using namespace direct3d_11_eg;
 	application app; // TODO: Command Line Parameters struct???
